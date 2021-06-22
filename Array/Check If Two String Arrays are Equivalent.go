5a6ab210cff42abc4c784729199e785c7be956aa
Parada <paradafaii28@gmail.com>
2021-06-22 10:29:53 -0400

    1662. Check If Two String Arrays are Equivalent

---
+++ Array/Check If Two String Arrays are Equivalent.go
@@ -0,0 +1,31 @@
+var arrayStringsAreEqual = function(word1, word2) {
+    //declare newWord1 = ''
+    //declare newWord2 = ''
+    //iterate through word1 
+        //concat word1[i] to newWord1 assign to newWord1
+    
+    //iterate through word2 
+        //concat word2[i] to newWord2 assign to newWord2
+    
+    //compare newWord1 === newWord2 
+        //if yes return true
+        //else return false
+    
+//     let newWord1 = '';
+//     let newWord2 = '';
+    
+//     for(let i = 0; i < word1.length; i++){
+//         newWord1 += word1[i]
+//     }
+    
+//     for(let i = 0; i < word2.length; i++){
+//         newWord2 += word2[i]
+//     }
+    
+//     return newWord1 === newWord2
+    
+    //////////    
+    //compare word1.join === word2.join
+    
+    return word1.join('') === word2.join('')
+};
