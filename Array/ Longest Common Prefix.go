61236b79354cb411442b8a56e24499a143dcafe7
Parada <paradafaii28@gmail.com>
2021-07-01 13:08:05 -0400

    14. Longest Common Prefix

---
+++ Array/ Longest Common Prefix.go
@@ -0,0 +1,27 @@
+var longestCommonPrefix = function(strs) {
+    //create prefix = ''
+        //looping through strs at index[0]
+            //set index[0][i] assign to character
+            //itrate through strs[j] by using i for pointing to that index
+                //compair if character !== strs[j][i]
+                    //return prefix
+                //else
+                    //prefix = prefix concat character
+    //return prefix
+    
+    let prefix = '';
+        for(let i = 0; i < strs[0].length; i++){
+            const character = strs[0][i]
+        
+            for(let j = 0; j < strs.length; j++){
+                //when you loop, you using i to refer to that index // j itrate to strs but i use for point to that index
+                // console.log(strs[j][i])
+                if(character !== strs[j][i]){
+                    return prefix
+                }
+            } 
+            prefix = prefix + character  
+        }
+    return prefix
+    
+};
